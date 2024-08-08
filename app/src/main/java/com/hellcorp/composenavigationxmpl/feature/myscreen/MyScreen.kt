fae1c2453ba8cd6de51ea628f32b3e30092dcfa1
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-08-09 00:26:44 +0300

    add ModalBottomSheet

--- app/src/main/java/com/hellcorp/composenavigationxmpl/feature/myscreen/MyScreen.kt
+++ app/src/main/java/com/hellcorp/composenavigationxmpl/feature/myscreen/MyScreen.kt
@@ -2,7 +2,6 @@ package com.hellcorp.composenavigationxmpl.feature.myscreen
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,10 +10,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
@@ -23,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.hellcorp.composenavigationxmpl.feature.dialog.SpecialDialog
 import kotlinx.coroutines.launch
 
@@ -83,8 +87,12 @@ fun MyScreen(paddingValues: PaddingValues) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FirstScreen() {
+    val scope = rememberCoroutineScope()
+    val bottomSheetState = rememberModalBottomSheetState()
+    val shouldShowBottomSheet = remember { mutableStateOf(false) }
     val shouldShowDialog = remember { mutableStateOf(false) }
 
     SpecialDialog(
@@ -93,27 +101,83 @@ fun FirstScreen() {
         onConfirmation = { shouldShowDialog.value = false },
     )
 
-    Column {
+    SpecialModalBottomSheet(
+        visible = shouldShowBottomSheet.value,
+        bottomSheetState = bottomSheetState,
+        onDismissRequest = { shouldShowBottomSheet.value = false },
+        hideBottomSheet = {
+            scope.launch { bottomSheetState.hide() }.invokeOnCompletion {
+                if (!bottomSheetState.isVisible) {
+                    shouldShowBottomSheet.value = false
+                }
+            }
+        },
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
         Text(
             text = "first page",
             modifier = Modifier
-                .fillMaxSize()
                 .background(Color.White)
-                .clickable {
-                    shouldShowDialog.value = true
-                }
         )
+        Button(
+            onClick = {
+                shouldShowDialog.value = true
+            }
+        ) {
+            Text(text = "Show dialog")
+        }
+        Button(
+            onClick = {
+                shouldShowBottomSheet.value = true
+            },
+        ) {
+            Text(text = "Show bottomsheet")
+        }
     }
 }
 
 @Composable
 fun SecondScreen() {
-    Column {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+            .background(Color.Yellow)
+    ) {
         Text(
             text = "second page",
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Yellow)
         )
     }
-} 
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SpecialModalBottomSheet(
+    visible: Boolean,
+    bottomSheetState: SheetState,
+    onDismissRequest: () -> Unit,
+    hideBottomSheet: () -> Unit,
+) {
+
+    if (visible)
+        ModalBottomSheet(
+            onDismissRequest = { onDismissRequest() },
+            sheetState = bottomSheetState,
+        ) {
+            Column(Modifier.fillMaxSize()) {
+                Button(
+                    onClick = {
+                        hideBottomSheet()
+                    }
+                ) {
+                    Text("Спрятать!")
+                }
+                Text(text = "Место для вашего контента")
+            }
+        }
+}
